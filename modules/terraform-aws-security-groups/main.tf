24ce91c9fce59ab40673c41e679d4c341833c367
Jerry Wang <j3rry.wan9@gmail.com>
2021-08-09 15:37:00 -0700

    add module for security groups

---
+++ modules/terraform-aws-security-groups/main.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group" "ec2" {
+  name = var.instance_security_group_name
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
